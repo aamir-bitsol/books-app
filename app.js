febe2770a891878ab1e6ff7721ddd7370ce8c024
Muhammad Aamir Zaman <aamir.zaman@bitsol.tech>
2023-04-13 09:31:51 +0500

    implemented the Crud operations in express JS

--- app.js
+++ app.js
@@ -1,37 +1,69 @@
 // import "./data.json"
 
+//setup express JS
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-// app.set('view engine', "ejs")
+// using json parser
+app.use(bodyParser.json());
 
 
+let books = [
+    {id:1, title:"LearnJs", author:"ABC"},
+    {id:2, title:"LearnNode", author:"XYZ"}
+]
 
 app.post('/',  (req, res) => {
     app.use('/', express.static('view'))
-    response={
-        Author: req.body.Author,
-        Title: req.body.Title,
+
+    book = {
+        id: books.length + 1,
+        author: req.body.author,
+        title: req.body.title,
     }
-    res.send(response);
+
+    books.push(book);
+    res.send(books[books.length -1]);
 })
 
 app.get('/', (req, res) => {
-    res.send('get req')
+    res.send(books);
 })
 
 app.get('/:id', (req, res) => {
-    res.send('get req')
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    
+    if(!book){
+        res.send("Book not found with given ID").status(404);
+    }
+    
+    res.send(book).status(200)
 })
 
 app.put('/:id', (req, res) => {
-    res.send('Got a PUT request at /user')
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    
+    if(!book){
+        res.send("Book not found with given ID").status(404);
+    }
+
+    book.author = req.body.author;
+    book.title = req.body.title;
+
+    res.send("Record has been updated successfully").status(200);
 })
 
 app.delete('/:id', (req, res) => {
-    res.send('Got a DELETE request at /del')
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    
+    if(!book){
+        res.send("Book not found with given ID").status(404);
+    }
+    
+    const book_index = books.indexOf(book);
+    books.splice(book, 1);
+    res.send("Record has been deleted successfully").status(200);
 })
 
 app.listen(3000);
